e6d5c346ad04901ce93a0c17c710d26d4a4ddfa1
Bishal Khanal <bishal.khanal87@gmail.com>
2022-03-22 16:14:04 -0700

    refactored to add components and fix error message/bootstrap issue

--- src/App.js
+++ src/App.js
@@ -1,70 +1,23 @@
-import React from 'react';
-import axios from 'axios';
 import './App.css';
+import Explorer from './Explorer.js'
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './location.css'
+import './explorer.css'
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cityData: {},
-      error: false,
-    }
-  }
 
-  handleCityData = async (e) => {
-    e.preventDefault()
-
-    try {
-
-      // get the data from the API
-      // let urlVal = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
-      // console.log(urlVal);
-      let cityValue = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
-      console.log(cityValue.data[0]);
-
-      // save the data into state
-      this.setState({
-        cityData: cityValue.data[0]
-      });
-    } catch (error) {
-      this.setState({
-        error: true,
-        errorMessage: `An error ocured: ${error.response.status}`
-     
-      })
-    
-    }
-    console.log(this.state.errorMessage);
-  };
-
-  handleCityInput = (e) => {
-    this.setState({
-      city: e.target.value
-    })
-  }
-  render() {
-    // let cityListItems = this.state.cityData.map()
-
+function App(){
+  
     return (
-      <>
-        <h1>City Explorer</h1>
-        <form onSubmit={this.handleCityData}>
-          <label>Enter a city:
-            <input type="text" onInput={this.handleCityInput} />
-            <button type="submit">Get city data</button>
-          </label>
-        </form>
-      <form>
-        {this.state.cityData.error ? <p>{this.state.errorMessage}</p> : <p>{this.state.cityData.display_name} {this.state.cityData.lat}  {this.state.cityData.lon}</p>}
-        </form>
+      <div>
 
-        <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=12`} alt = 'Map'/>
-        
-      
-      </>
-    );
-  }
+     
+        <Explorer />
+        </div>
+       
+        );
 }
 
-export default App;
+
+        export default App;
